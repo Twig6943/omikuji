332b472f702adfd58bce66ea49054616e4d4a713
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2026-05-13 20:43:32 +0300

    Update make-appimage.sh

--- packaging/make-appimage.sh
+++ packaging/make-appimage.sh
@@ -3,6 +3,8 @@
 set -eu
 
 ARCH=$(uname -m)
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
 VERSION=$(pacman -Q omikuji-git | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
@@ -11,10 +13,15 @@ export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}
 export ICON=/usr/share/icons/hicolor/512x512/apps/io.github.reakjra.omikuji.png
 export DESKTOP=/usr/share/applications/io.github.reakjra.omikuji.desktop
 
-# Deploy dependencies
-quick-sharun /usr/bin/omikuji
+#Remove leftovers
+rm -rf AppDir dist
 
-# Additional changes can be done in between here
+# ADD LIBRARIES
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+
+# Deploy dependencies
+./quick-sharun /usr/bin/omikuji
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
